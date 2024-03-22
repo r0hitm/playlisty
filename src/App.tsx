73ca978071495e4b264dded200220d53be663101
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-22 15:00:38 +0530

    test-run spotify-ts: succesfully fetched user profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import { useState } from "react";
+import { useSpotify } from "./hooks/useSpotify";
+import {
+    Scopes /*SearchResults, SpotifyApi*/,
+    UserProfile,
+} from "@spotify/web-api-ts-sdk";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const sdk = useSpotify(
+        import.meta.env.VITE_SPOTIFY_CLIENT_ID as string,
+        import.meta.env.VITE_REDIRECT_TARGET as string,
+        Scopes.userDetails
+    );
+
+    const [user, setUser] = useState<UserProfile | null>(null);
+    useEffect(() => {
+        (async () => {
+            try {
+                const currentUser = await sdk?.currentUser.profile();
+                currentUser && setUser(currentUser);
+            } catch (err: unknown) {
+                console.log(
+                    "An error occured while fetching user profile",
+                    err
+                );
+            }
+        })();
+    });
 
     return (
         <>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount(count => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <h1>こんにちは、頑張っている！</h1>
+            <p>{user?.display_name}</p>
+            <p>{user?.email}</p>
+            <p>{user?.country}</p>
+            {user?.images ? (
+                <img src={user?.images[0].url} alt="profile" />
+            ) : null}
         </>
     );
 }

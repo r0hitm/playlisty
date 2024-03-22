d3fe372c60116779147e2bc8b0e456b6b575d0b6
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-22 20:34:56 +0530

    WIP: a mess

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useSpotify } from "./hooks/useSpotify";
 import {
     Scopes /*SearchResults, SpotifyApi*/,
-    UserProfile,
 } from "@spotify/web-api-ts-sdk";
-import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -13,30 +11,36 @@ function App() {
         Scopes.userDetails
     );
 
-    const [user, setUser] = useState<UserProfile | null>(null);
-    useEffect(() => {
-        (async () => {
-            try {
-                const currentUser = await sdk?.currentUser.profile();
-                currentUser && setUser(currentUser);
-            } catch (err: unknown) {
-                console.log(
-                    "An error occured while fetching user profile",
-                    err
-                );
-            }
-        })();
-    });
-
     return (
         <>
-            <h1>こんにちは、頑張っている！</h1>
-            <p>{user?.display_name}</p>
-            <p>{user?.email}</p>
-            <p>{user?.country}</p>
-            {user?.images ? (
-                <img src={user?.images[0].url} alt="profile" />
-            ) : null}
+            <header>
+                <img src="/Logo.webp" alt="Logo" className="logo" />
+                <ul>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                </ul>
+                {sdk ? (
+                    <button
+                        onClick={() => {
+                            console.log("TODO: Log out");
+                        }}
+                    >
+                        Log out
+                    </button>
+                ) : (
+                    <button
+                        onClick={() => {
+                            console.log("TODO: Log in");
+                        }}
+                    >
+                        Log in
+                    </button>
+                )}
+            </header>
         </>
     );
 }

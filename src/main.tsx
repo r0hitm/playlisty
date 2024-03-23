3ba9489d7d5efeff05ad61eb51cd6a1602a232c8
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-23 21:57:38 +0530

    WIP: started adding react-router routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { Root } from "./Root.tsx";
 
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Root />, // jsx or component to be rendered on this path
+            //   errorPage: // component  to render when error (see below)
+            //   children: [/* nested routes */], // to render use <Outlet /> in <App />
+        },
+        {
+            path: "/contact/:id", // dynacmic url
+            // ....
+        },
+        // ... rest of the routes
+    ]
+    // basename: "/<REPO>" // if not being deployed to root of the website
+);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Root />
+        {/* <Root /> */}
+        <RouterProvider router={router} />
     </React.StrictMode>
 );

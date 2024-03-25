d01467f20e31a48a4b4d09628fdac545b4fdd331
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-25 15:58:58 +0530

    removed uneeded lines

--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,4 @@
-/* eslint-disable @typescript-eslint/require-await */
-
-/// 🔴^--- Remove the above line after implementing customRedirectionStrategy properly
-
-import { IRedirectionStrategy, SdkOptions, Scopes } from "@spotify/web-api-ts-sdk";
-// const defaultConfig: SdkConfiguration = {
-//     fetch: (req: RequestInfo | URL, init: RequestInit | undefined) => fetch(req, init),
-//     beforeRequest: (_: string, __: RequestInit) => { },
-//     afterRequest: (_: string, __: RequestInit, ___: Response) => { },
-//     deserializer: new DefaultResponseDeserializer(),
-//     responseValidator: new DefaultResponseValidator(),
-//     errorHandler: new NoOpErrorHandler(),
-//     redirectionStrategy: new DocumentLocationRedirectionStrategy(),
-//     cachingStrategy: isBrowser
-//         ? new LocalStorageCachingStrategy()
-//         : new InMemoryCachingStrategy()
-// };
-
-export default class customRedirectionStrategy implements IRedirectionStrategy {
-    public async redirect(targetUrl: string | URL): Promise<void> {
-        document.location = targetUrl.toString();
-    }
-
-    public async onReturnFromRedirect(): Promise<void> {
-        console.log("Returned from redirect");
-        // Figure out how to successfully log in the user
-        // rn after redirect the state is lost
-    }
-}
+import { SdkOptions, Scopes } from "@spotify/web-api-ts-sdk";
 
 export const opts: SdkOptions = {
     beforeRequest: (url: string, init: RequestInit) => {
@@ -35,9 +7,7 @@ export const opts: SdkOptions = {
     afterRequest: (url: string, init: RequestInit, res: Response) => {
         console.log("After request", url, init, res);
     },
-    redirectionStrategy: new customRedirectionStrategy(),
 };
-
 
 export const appScopes = [
     ...Scopes.userDetails,

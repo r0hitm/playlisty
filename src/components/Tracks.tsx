24c13d49a09425ebf6eb42e74d4fa3d8e5779456
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-29 15:06:25 +0530

    update track style + added thumbnail

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -23,107 +23,10 @@ export default function Tracks({
     loading: boolean;
     handleTrackSelect: (track: Track | null) => void;
 }) {
-    // const sdk = useSdk();
-
-    // const [allTracksLoaded, setAllTracksLoaded] = useState<boolean>(false);
-    // const [isLoading, setIsLoading] = useState<boolean>(false);
-
-    // effect for the first fetch
-    // useEffect(() => {
-    //     if (!sdk || !activePlaylist) {
-    //         return;
-    //     }
-
-    //     (async () => {
-    //         try {
-    //             setIsLoading(true);
-    //             setAllTracksLoaded(false);
-    //             const playlistTracks = await sdk.playlists.getPlaylistItems(
-    //                 activePlaylist.id
-    //             );
-    //             const allItems = playlistTracks.items;
-    //             setTracks({
-    //                 ...playlistTracks,
-    //                 allItems,
-    //             });
-    //             if (!playlistTracks.next) {
-    //                 setAllTracksLoaded(true);
-    //             }
-    //         } catch (error) {
-    //             console.error(error);
-    //         } finally {
-    //             setIsLoading(false);
-    //         }
-    //     })();
-
-    //     return () => {
-    //         setTracks(null);
-    //         handleTrackSelect(null);
-    //     };
-    // }, [activePlaylist]);
-
-    // const loadMoreTracks = async () => {
-    //     if (!sdk || !tracks?.next) {
-    //         return;
-    //     }
-
-    //     try {
-    //         setIsLoading(true);
-    //         const accessToken = await sdk.getAccessToken();
-    //         if (!accessToken) {
-    //             throw new Error("No access token");
-    //         }
-    //         console.log("next", tracks.next);
-    //         console.log("accessToken", accessToken);
-    //         const response = await fetch(tracks.next, {
-    //             headers: {
-    //                 Authorization: `Bearer ${accessToken.access_token}`,
-    //             },
-    //         });
-    //         if (!response.ok) {
-    //             throw new Error("Failed to fetch");
-    //         }
-    //         console.log("response", response);
-    //         const nextTracks = (await response.json()) as Page<
-    //             PlaylistedTrack<Track>
-    //         >;
-    //         console.log("nextTracks", nextTracks);
-    //         const allItems = [...tracks.allItems, ...nextTracks.items];
-    //         setTracks({
-    //             ...nextTracks,
-    //             allItems,
-    //         });
-
-    //         if (!nextTracks.next) {
-    //             setAllTracksLoaded(true);
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     } finally {
-    //         setIsLoading(false);
-    //     }
-    // };
-
     return (
         <div className="tracks-component">
             <h2>Songs in this playlist</h2>
             <ul>
-                {/* {tracks?.tracks.allItems.map((track, index) => (
-                    <li
-                        key={index}
-                        className={
-                            selectedTrack?.id === track.track.id
-                                ? "selected"
-                                : ""
-                        }
-                        onClick={() => handleTrackSelect(track.track)}
-                    >
-                        <span className="title">{track.track.name}</span>
-                        <span className="artist">
-                            {track.track.artists[0].name}
-                        </span>
-                    </li>
-                ))} */}
                 {activePlaylist?.tracks.allItems.map((track, index) => (
                     <li
                         key={index}
@@ -132,10 +35,18 @@ export default function Tracks({
                         }
                         onClick={() => handleTrackSelect(track.track)}
                     >
-                        <span className="title">{track.track.name}</span>
-                        <span className="artist">
-                            {track.track.artists[0].name}
-                        </span>
+                        <span className="index">{index + 1}. </span>
+                        <img
+                            className="thumbnail"
+                            src={track.track?.album?.images?.[2]?.url ?? ""}
+                            alt={track.track?.name}
+                        />
+                        <div className="song-info">
+                            <span className="title">{track.track.name}</span>
+                            <span className="artist">
+                                {track.track.artists[0].name}
+                            </span>
+                        </div>
                     </li>
                 ))}
             </ul>

e9995f310ea05d833a54ee00daa806d0d377e8ea
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-31 17:39:07 +0530

    added animated loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,8 @@
-import "./Loading.css"
+import "./Loading.css";
 export default function Loading() {
-    return <div className="loading">Loading...</div>;
+    return (
+        <div className="loading">
+            <img src="./loader.svg"></img>
+        </div>
+    );
 }

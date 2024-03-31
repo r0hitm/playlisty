d664dc2610418863dd777ebf3276edd754ae790b
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-31 19:12:37 +0530

    song thumbnails now load lazily

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -2,6 +2,7 @@ import { Track } from "@spotify/web-api-ts-sdk";
 import "./Tracks.css";
 import { PlaylistTracks } from "../customInterfaces";
 import Loading from "./Loading";
+import { useEffect, useRef } from "react";
 
 export default function Tracks({
     activePlaylist,
@@ -14,6 +15,25 @@ export default function Tracks({
     loading: boolean;
     handleTrackSelect: (track: Track | null) => void;
 }) {
+    const imgRefs = useRef<HTMLImageElement[]>([]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target as HTMLImageElement;
+                    img.src = img.dataset.src ?? "";
+                }
+            });
+        });
+
+        imgRefs.current.forEach(img => img && observer.observe(img));
+
+        return () => {
+            imgRefs.current.forEach(img => img && observer.unobserve(img));
+        };
+    }, [activePlaylist]);
+
     return (
         <div className="tracks-component">
             <h2>Songs in this playlist</h2>
@@ -29,7 +49,10 @@ export default function Tracks({
                         <span className="index">{index + 1}. </span>
                         <img
                             className="thumbnail"
-                            src={track.track?.album?.images?.[2]?.url ?? ""}
+                            ref={img => imgRefs.current.push(img!)}
+                            data-src={
+                                track.track?.album?.images?.[2]?.url ?? ""
+                            }
                             alt={track.track?.name}
                         />
                         <div className="song-info">

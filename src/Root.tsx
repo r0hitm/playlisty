c145b402017998abd50fc72317f38714f10ec9c0
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-03-31 13:07:44 +0530

    Root welcome message only displays if user not logged in and on /

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Header } from "./components/Header";
 import { useSpotify } from "./hooks/useSpotify";
 import { FloatingFooter } from "./components/FloatingFooter";
+import "./article-ishStyles.css";
 
 export default function Root() {
     const { sdk, logIn, logOut } = useSpotify();
+    const location = useLocation();
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
@@ -19,17 +21,25 @@ export default function Root() {
             <Header
                 logInStatus={sdk !== null}
                 login={() => {
-                    void logIn();
+                    void logIn(); 
                 }}
                 logout={logOut}
             />
             <Outlet context={sdk} />
             <FloatingFooter />
-            {sdk ? (
-                <Navigate to="/app" replace />
-            ) : (
-                <p>Get started with logging in with Spotify!</p>
-            )}
+            {sdk ? <Navigate to="/app" replace /> : null}
+            {!sdk && location.pathname === "/" ? (
+                <article>
+                    <p>
+                        Track your songs across playlists in a single view! 🎶
+                    </p>
+                    <p>
+                        Hard to keep track of which songs are in which playlist?
+                        This app will help you with that! 📝
+                    </p>
+                    <p>Get started by logging in with your Spotify! 🎵</p>
+                </article>
+            ) : null}
         </>
     );
 }
